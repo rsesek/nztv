24876a6232195372d4eea7fd74714c7f6436feda
Robert Sesek <rsesek@bluestatic.org>
2011-05-17 01:10:58 -0400

    Add support for relocating unpacked files into a sorted folder structure.
    This uses fuzzy title matching of directories in a set root folder. This is
    controlled by the NZTV_UNPACK_SORTED environment variable.

--- unpack-download.php
+++ unpack-download.php
@@ -65,6 +65,50 @@ function GetNewFilename($filename) {
   return NULL;
 }
 
+// This function will try to match |$title| to a directory in |$root|.
+function FuzzyMatchTitle($root, $title) {
+  $tokenizer = function($s) {
+    return explode(' ', strtoupper($s));
+  };
+  $word_filter = function($e) {
+    return !in_array($e, array(
+      'A',
+      'AN',
+      'THE',
+    ));
+  };
+
+  // Iterate through each of the directories in |$root| and try to match it to
+  // the source.
+  $source_words = $tokenizer($title);
+  $it = new DirectoryIterator($root);
+  foreach ($it as $file) {
+    if (!$file->IsDir() || $file->IsDot())
+      continue;
+
+    $dirname_words = $tokenizer($file->GetFilename());
+
+    // Compute the difference. In order to a meaningful match, do a symmetric
+    // set difference. That is, all the objects that are either in just the
+    // source or just the target. Words that are common between the two are
+    // excluded.
+    $delta1 = array_diff($source_words, $dirname_words);
+    $delta2 = array_diff($dirname_words, $source_words);
+    $delta = array_merge($delta1, $delta2);
+    // After computing the delta, filter out any words in a title that are
+    // generally irrelevant.
+    $delta = array_filter($delta, $word_filter);
+
+    // If the delta set is empty, then the titles match.
+    if (count($delta) == 0) {
+      return $file->GetPathname();
+    }
+  }
+
+  // No match was found.
+  return NULL;
+}
+
 function Main($directory_name) {
   global $file_extensions, $remove_file_extensions;
 
@@ -89,6 +133,29 @@ function Main($directory_name) {
 
       // Create new fully qualified path.
       $new_path = dirname(realpath($file->GetPath()));
+
+      // If set up to sort into show and season folders, move this there.
+      if (isset($_SERVER['NZTV_UNPACK_SORTED'])) {
+        // Explode name of |Show - SxE - Title|.
+        $showname_parts = explode(' - ', $new_name);
+        if (count($showname_parts) >= 2) {
+          $show_season = explode('x', $showname_parts[1]);
+          $title_folder = FuzzyMatchTitle($_SERVER['NZTV_UNPACK_SORTED'],
+                                          $showname_parts[0]);
+          // If the show uses episodic naming and a title folder was found, then
+          // schedule the move into the proper season folder.
+          if (count($show_season) == 2 && $title_folder) {
+            $test_new_path = $title_folder . '/' .
+                             sprintf('Season %d', $show_season[0]);
+            // Final sanity check that the location of the move has been found.
+            if (file_exists($test_new_path) && is_dir($test_new_path)) {
+              $new_path = $test_new_path;
+            }
+          }
+        }
+      }
+
+      // Create the full name.
       $new_pathname = $new_path . '/' . $new_name;
 
       // Create thunk.
